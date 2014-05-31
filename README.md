d45e4ea5ac7419450230f4cdf1ab87ca889eb368
Eddy Beaupre <eddy@beaupre.biz>
2014-05-30 20:07:18 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 ap6210
 ======
 
-Wifi and Bluetooth driver for CubieTruck
+Wifi and Bluetooth driver for CubieTruck.
+
+This patch should be applied to the root of the linux-sunxi kernel's source :
+
+    # patch -p1 < linux-sunxi-ap6210.diff
+
+To load the module, add the following lines to /etc/modules
+
+    ap6210_gpio_wifi
+    ap6210_gpio_bt
+    ap6210
+
+Feel free to improve the code and submit bugfix...
+
---
+++ drivers/net/wireless/ap6210/ap6210_gpio_bt.c
@@ -0,0 +1,163 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/rfkill.h>
+#include <linux/delay.h>
+#include <linux/platform_device.h>
+#include <plat/sys_config.h>
+
+#define BT_INF(...)	pr_info("[ap6210_gpio_bt]: "__VA_ARGS__)
+#define BT_WRN(...)	pr_warning("[ap6210_gpio_bt]: "__VA_ARGS__)
+
+#if (defined CONFIG_MMC)
+#include "ap6210_gpio.h"
+#else
+static __inline int ap6210_gpio_wifi_get_mod_type(void)
+{
+	BT_WRN("%s : not implemented!\n", __FUNCTION__ );
+	return 0;
+}
+static __inline int ap6210_gpio_wifi_gpio_ctrl(char* name, int level)
+{
+	BT_WRN("%s : not implemented!\n", __FUNCTION__ );
+	return -1;
+}
+#endif
+
+static const char bt_name[] = "bcm40183";
+static struct rfkill *sw_rfkill;
+static int bt_used;
+
+static int rfkill_set_power(void *data, bool blocked)
+{
+	unsigned int mod_sel = ap6210_gpio_wifi_get_mod_type();
+    
+	BT_INF("rfkill set power %s\n", ( blocked ? "blocked" : "unblocked" ));
+    
+	switch (mod_sel)
+	{
+        case 2: /* bcm40183 */
+		if (!blocked) {
+			ap6210_gpio_wifi_gpio_ctrl("bcm40183_bt_regon", 1);
+			ap6210_gpio_wifi_gpio_ctrl("bcm40183_bt_rst", 1);
+		} else {
+			ap6210_gpio_wifi_gpio_ctrl("bcm40183_bt_rst", 0);
+			ap6210_gpio_wifi_gpio_ctrl("bcm40183_bt_regon", 0);
+		}
+		BT_INF("Using %s configuration.\n", ap6210_gpio_wifi_get_name(mod_sel) );
+		break;
+        case 3: /* realtek rtl8723as */
+		if (!blocked) {
+			ap6210_gpio_wifi_gpio_ctrl("rtk_rtl8723as_bt_dis", 1);
+		} else {
+			ap6210_gpio_wifi_gpio_ctrl("rtk_rtl8723as_bt_dis", 0);
+		}
+		BT_INF("Using %s configuration.\n", ap6210_gpio_wifi_get_name(mod_sel) );
+		break;            
+        case 7: /* ap6210 */
+        case 8: /* ap6330 */
+		if (!blocked) {
+			ap6210_gpio_wifi_gpio_ctrl("ap6xxx_bt_regon", 1);
+		} else {
+			ap6210_gpio_wifi_gpio_ctrl("ap6xxx_bt_regon", 0);
+		}
+		BT_INF("Using %s configuration.\n", ap6210_gpio_wifi_get_name(mod_sel) );
+		break;    
+	case 10: /* realtek rtl8723au */
+		if (!blocked) {
+			ap6210_gpio_wifi_gpio_ctrl("rtl8723au_bt", 1);
+		} else {
+			ap6210_gpio_wifi_gpio_ctrl("rtl8723au_bt", 0);
+		}
+		BT_INF("Using %s configuration.\n", ap6210_gpio_wifi_get_name(mod_sel) );
+		break;
+        default:
+		BT_WRN("no bluetooth module matched.\n" );
+	}
+    
+	msleep(10);
+	return 0;
+}
+
+static struct rfkill_ops sw_rfkill_ops = {
+	.set_block = rfkill_set_power,
+};
+
+static int sw_rfkill_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+
+	sw_rfkill = rfkill_alloc(bt_name, &pdev->dev, 
+				 RFKILL_TYPE_BLUETOOTH, &sw_rfkill_ops, NULL);
+	if (unlikely(!sw_rfkill)) {
+		BT_WRN("Unable to alocate rfkill structure.\n" );
+		return -ENOMEM;
+		}
+
+	ret = rfkill_register(sw_rfkill);
+	if (unlikely(ret)) {
+		BT_WRN("Unable to register rfkill structure.\n" );
+		rfkill_destroy(sw_rfkill);
+	}
+	BT_INF("rfkill structure registered successfully.\n" );
+	return ret;
+}
+
+static int sw_rfkill_remove(struct platform_device *pdev)
+{
+	if (likely(sw_rfkill)) {
+		rfkill_unregister(sw_rfkill);
+		rfkill_destroy(sw_rfkill);
+		BT_INF("rfkill structure removed successfully.\n" );
+	}
+	
+	return 0;
+}
+
+static struct platform_driver sw_rfkill_driver = {
+	.probe = sw_rfkill_probe,
+	.remove = sw_rfkill_remove,
+	.driver = { 
+		.name = "sunxi-rfkill",
+		.owner = THIS_MODULE,
+	},
+};
+
+static struct platform_device sw_rfkill_dev = {
+	.name = "sunxi-rfkill",
+};
+
+static int __init sw_rfkill_init(void)
+{
+	if (SCRIPT_PARSER_OK != script_parser_fetch("bt_para", "bt_used", &bt_used, 1)) {
+		BT_WRN("parse bt_used failed in script.fex.\n" );
+		return -1;
+	}
+
+	if (!bt_used) {
+		BT_WRN("bluetooth is disable in script.fex.\n" );
+		return 0;
+	}
+
+	platform_device_register(&sw_rfkill_dev);
+	BT_INF("platform device registered successfully.\n" );
+	return platform_driver_register(&sw_rfkill_driver);
+}
+
+static void __exit sw_rfkill_exit(void)
+{
+	if (!bt_used) {
+		BT_INF("exit no bt used in configuration\n" );
+		return ;
+	}
+
+	platform_device_unregister(&sw_rfkill_dev);
+	platform_driver_unregister(&sw_rfkill_driver);
+}
+
+module_init(sw_rfkill_init);
+module_exit(sw_rfkill_exit);
+
+MODULE_DESCRIPTION("sunxi-rfkill driver");
+MODULE_AUTHOR("Aaron.magic<mgaic@reuuimllatech.com>");
+MODULE_LICENSE("GPL");
+
